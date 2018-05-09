5a9afd0e1a74e37611c48426c4dcaa2801f93647
REDMOND\v-hichr <v-hichr@microsoft.com>
2018-05-09 16:38:02 -0700

    Added Routing and Dashboard stuff

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,23 +9,15 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstrom'
-  }
   heroes: Hero[];
-  selectedHero: Hero;
   
   constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  ngOnInit() {
+    this.getHeroes();
   }
   
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-  ngOnInit() {
-    this.getHeroes();
   }
 }
